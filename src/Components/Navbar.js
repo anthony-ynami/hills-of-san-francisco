1122934c8bab7dd032846c4679324210d8798f1a
anthony-ynami <anthony.ynami@gmail.com>
2021-02-08 18:55:39 -0800

    updated read-me and links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
             <div className="links">
                 <NavLink exact to="/" classname="navlink" activeClassName="selected">Home</NavLink>
                 <NavLink to="/project" classname="navlink" activeClassName="selected">Project</NavLink>
-                <a classname="navlink" href="/#">Git</a>
-                <a classname="navlink" href="/#">Portfolio</a>
+                <a classname="navlink" href="https://github.com/anthony-ynami/hills-of-san-francisco">Git</a>
+                <a classname="navlink" href="ynami.xyz">Portfolio</a>
             </div>
         </nav>
     );
